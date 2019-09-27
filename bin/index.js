055c40a05ed9e74e90c9e0bb49d5d8f14f649fde
Mike Kruk <tamagokun@gmail.com>
2019-09-27 06:42:47 -0400

    properly parse full package name when copying packages

--- bin/index.js
+++ bin/index.js
@@ -1,77 +1,87 @@
 #!/usr/bin/env node
 
-const { resolve } = require('path')
-const { execSync } = require('child_process')
-const archiver = require('archiver-promise')
-const glob = require('fast-glob')
-const { existsSync, copySync, removeSync, ensureDirSync } = require('fs-extra')
-const resolveDependencies = require('../lib/resolveDependencies')
+const { resolve } = require("path");
+const { execSync } = require("child_process");
+const archiver = require("archiver-promise");
+const glob = require("fast-glob");
+const { existsSync, copySync, removeSync, ensureDirSync } = require("fs-extra");
+const resolveDependencies = require("../lib/resolveDependencies");
 
-const rootDir = process.cwd()
-const folder = process.argv[2]
+const rootDir = process.cwd();
+const folder = process.argv[2];
 
 if (!folder) {
-  throw new Error('Please supply a folder')
+  throw new Error("Please supply a folder");
 }
 
-const workspacePkg = require(resolve(rootDir, 'package.json'))
+const workspacePkg = require(resolve(rootDir, "package.json"));
 if (!workspacePkg.workspaces) {
-  throw new Error('You must specify a `workspaces` field in your workspaces\'s package.json.')
+  throw new Error(
+    "You must specify a `workspaces` field in your workspaces's package.json."
+  );
 }
 
-const localPackages = glob.sync(workspacePkg.workspaces, {cwd: rootDir, onlyDirectories: true})
-const pkgDir = localPackages.find(dir => dir.split('/').pop() === folder)
+const localPackages = glob.sync(workspacePkg.workspaces, {
+  cwd: rootDir,
+  onlyDirectories: true
+});
+const pkgDir = localPackages.find(dir => dir.split("/").pop() === folder);
 
 if (!pkgDir) {
-  throw new Error(`Folder \`${folder}\` was not found.`)
+  throw new Error(`Folder \`${folder}\` was not found.`);
 }
 
-const pkg = require(resolve(rootDir, pkgDir, 'package.json'))
+const pkg = require(resolve(rootDir, pkgDir, "package.json"));
 
-const localModules = []
+const localModules = [];
 for (const dir of localPackages) {
   try {
-    const pkg = require(resolve(rootDir, dir, 'package.json'))
-    localModules.push(pkg)
+    const pkg = require(resolve(rootDir, dir, "package.json"));
+    localModules.push(pkg);
   } catch (e) {}
 }
 
-const buildDir = resolve(rootDir, '_build')
-removeSync(buildDir)
+const buildDir = resolve(rootDir, "_build");
+removeSync(buildDir);
 
 const main = async () => {
   // copy package to build folder
-  copySync(resolve(rootDir, pkgDir), buildDir)
-  ensureDirSync(resolve(rootDir, pkgDir, 'node_modules'))
+  copySync(resolve(rootDir, pkgDir), buildDir);
+  ensureDirSync(resolve(rootDir, pkgDir, "node_modules"));
 
   // build
   if (pkg.scripts && pkg.scripts.build) {
-    execSync('yarn build', {cwd: buildDir})
+    execSync("yarn build", { cwd: buildDir });
   }
 
   // resolve dependencies
-  const deps = await resolveDependencies(pkg.dependencies, localModules)
+  const deps = await resolveDependencies(pkg.dependencies, localModules);
 
   // copy dependencies to node_modules
   deps
     .filter((value, index, self) => self.indexOf(value) === index)
     .map(dep => {
-      const dirName = dep.split('@').shift()
-      const dir = resolve(rootDir, 'node_modules', dirName)
-      if (!existsSync(dir)) return
+      const versionIndex = dep.lastIndexOf("@");
+      const dirName = dep.substr(0, versionIndex);
+      const dir = resolve(rootDir, "node_modules", dirName);
+      if (!existsSync(dir)) return;
 
-      copySync(dir, resolve(buildDir, 'node_modules', dirName), {dereference: true})
-    })
+      copySync(dir, resolve(buildDir, "node_modules", dirName), {
+        dereference: true
+      });
+    });
 
   // package into zip
-  const zip = archiver(resolve(rootDir, `${pkg.name.replace('/', '-')}.zip`), {store: true})
-  zip.directory(buildDir, false)
-  await zip.finalize()
+  const zip = archiver(resolve(rootDir, `${pkg.name.replace("/", "-")}.zip`), {
+    store: true
+  });
+  zip.directory(buildDir, false);
+  await zip.finalize();
 
   // remove build folder
-  removeSync(buildDir)
-}
+  removeSync(buildDir);
+};
 
 main()
   .then(() => process.exit())
-  .catch((e) => console.error(e) && process.exit(1))
+  .catch(e => console.error(e) && process.exit(1));
